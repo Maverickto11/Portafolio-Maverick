8fd7f8231405d93591775b706ef5d9310d83d847
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-11-08 18:39:27 -0500

    component de proyectos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from "./inicio/inicio.component";
 import { AcercaDeMiComponent } from "./acerca-de-mi/acerca-de-mi.component";
 import { HabilidadesComponent } from "./habilidades/habilidades.component";
+import { ProyectosComponent } from "./proyectos/proyectos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent, AcercaDeMiComponent, HabilidadesComponent],
+  imports: [RouterOutlet, InicioComponent, AcercaDeMiComponent, HabilidadesComponent, ProyectosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/proyectos/proyectos.component.ts
@@ -0,0 +1,64 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './proyectos.component.html',
+  styleUrl: './proyectos.component.css'
+})
+export class ProyectosComponent {
+  projects = [
+    {
+      title: 'Trailer de películas',
+      link: 'https://trailers-maverick.netlify.app',
+      image: '/assets/img/trailers.png',
+      techIcons: [
+        { src: 'https://angular.io/assets/images/logos/angular/angular.svg', alt: 'Angular Icon' },
+      ],
+    },
+    {
+      title: 'Parrilla Eléctrica',
+      link: 'https://parrilla-electrica.web.app/',
+      image: '/assets/img/parrila-electrica.png',
+      techIcons: [
+        { src: 'https://angular.io/assets/images/logos/angular/angular.svg', alt: 'Angular Icon' },
+      ],
+    },
+    {
+      title: 'Mini Ecommerce',
+      link: 'https://ecommerce-maverick.netlify.app',
+      image: '/assets/img/ecommerce.png',
+      techIcons: [
+        { src: 'https://angular.io/assets/images/logos/angular/angular.svg', alt: 'Angular Icon' },
+        { src: '/assets/iconos/net.png', alt: 'Spring Boot Icon' },
+        { src: '/assets/iconos/sqlServer.png', alt: 'MySQL Workbench Icon' },
+      ],
+    },
+    {
+      title: 'Diseño de app para Tienda',
+      link: 'https://www.figma.com/proto/Rf2XwR291XgcBsxzbs3ZCe/FIGMA_TAREA',
+      image: '/assets/img/figma-tienda.png',
+      techIcons: [
+        { src: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg', alt: 'Figma Icon' },
+      ],
+    },
+    {
+      title: 'Diseño de app para Animales',
+      link: 'https://www.figma.com/proto/sdOkZkDqUHhy6vOU3E0Hvz/Proyecto-UX-(Copy)',
+      image: '/assets/img/app-VetCuidados.png',
+      techIcons: [
+        { src: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg', alt: 'Figma Icon' },
+      ],
+    },
+    {
+      title: 'Visas Flash: Obtén tu visa',
+      link: 'https://visasflash4.godaddysites.com/',
+      image: '/assets/img/visasflash.png',
+      techIcons: [
+        { src: '/assets/iconos/wordpress.png', alt: 'WordPress Icon' },
+      ],
+    },
+  ];
+}
